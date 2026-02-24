1c8807cb109758fd985c6e2b8382f8645fe462f5
Squarecat-meow <yozumina@serafuku.moe>
2026-02-24 20:29:36 +0900

    웹훅 준비

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,12 +4,12 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const secret = req.nextUrl.searchParams.get('secret');
 
-  if (secret !== process.env.NOTION_TOKEN) {
-    return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
-  }
+  // if (secret !== process.env.NOTION_TOKEN) {
+  //   return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
+  // }
 
   // 'posts' 태그가 달린 모든 캐시를 즉시 만료시킴
-  revalidateTag('posts', 'max');
+  // revalidateTag('posts', 'max');
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ secret });
 }
