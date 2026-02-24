1954effc1a471c2218646fff66ea4778a990603e
Squarecat-meow <yozumina@serafuku.moe>
2026-02-24 20:08:33 +0900

    fix(gallery, post): Dynamic Route fetch에 Suspense 추가

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -3,22 +3,28 @@ import { INotionGallery } from '@/types/notion';
 import { NotionAPI } from 'notion-client';
 import { ExtendedRecordMap } from 'notion-types';
 import GalleryPage from './componenets/gallery-page';
+import { Suspense } from 'react';
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
+  return (
+    <Suspense fallback={<div>포스트를 불러오는 중...</div>}>
+      <GalleryContent params={params} />
+    </Suspense>
+  );
+}
+
+async function GalleryContent({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+
   const notionAPI = new NotionAPI();
   const [properties, recordMap] = (await Promise.all([
     notion.pages.retrieve({ page_id: id }),
     notionAPI.getPage(id),
   ])) as [INotionGallery, ExtendedRecordMap];
 
-  return (
-    <>
-      <GalleryPage post={properties} recordMap={recordMap} />
-    </>
-  );
+  return <GalleryPage post={properties} recordMap={recordMap} />;
 }
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -3,22 +3,28 @@ import { INotionPage } from '@/types/notion';
 import PostPage from './components/post-page';
 import { NotionAPI } from 'notion-client';
 import { ExtendedRecordMap } from 'notion-types';
+import { Suspense } from 'react';
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
+  return (
+    <Suspense fallback={<div>포스트를 불러오는 중...</div>}>
+      <PostContent params={params} />
+    </Suspense>
+  );
+}
+
+async function PostContent({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+
   const notionAPI = new NotionAPI();
   const [properties, recordMap] = (await Promise.all([
     notion.pages.retrieve({ page_id: id }),
     notionAPI.getPage(id),
   ])) as [INotionPage, ExtendedRecordMap];
 
-  return (
-    <>
-      <PostPage post={properties} recordMap={recordMap} />
-    </>
-  );
+  return <PostPage post={properties} recordMap={recordMap} />;
 }
