abd8d1986566e60f6060c50ebdbf299f431c267e
Squarecat-meow <yozumina@serafuku.moe>
2026-02-24 20:25:21 +0900

    revalidate 경로 수정

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const secret = req.nextUrl.searchParams.get('secret');
 
-  if (secret !== process.env.MY_REVALIDATE_TOKEN) {
+  if (secret !== process.env.NOTION_TOKEN) {
     return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
   }
 
