0fca46fd82a553c79e20fcb95df57da20ad9fe27
Squarecat-meow <yozumina@serafuku.moe>
2026-02-25 11:18:40 +0900

    동적 메타데이터 추가

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -4,6 +4,39 @@ import PostPage from './components/post-page';
 import { NotionAPI } from 'notion-client';
 import { ExtendedRecordMap } from 'notion-types';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+  const { id } = await params;
+  const res = (await notion.pages.retrieve({ page_id: id })) as INotionPage;
+
+  return {
+    title: `${res.properties.제목.title[0].plain_text} | Mina's Garden`,
+    description: '요즈미나의 작은 정원이에요(또는 그냥 블로그라고도 불러요)!',
+    openGraph: {
+      title: `${res.properties.제목.title[0].plain_text} | Mina's Garden`,
+      description: '요즈미나의 작은 정원이에요(또는 그냥 블로그라고도 불러요)!',
+      url: `/posts/${res.id}`,
+      images: [
+        {
+          url: res.cover
+            ? res.cover.type === 'file'
+              ? res.cover.file.url
+              : res.cover.external.url
+            : '',
+          width: 1200,
+          height: 630,
+          alt: 'og-image',
+        },
+      ],
+      type: 'website',
+    },
+  };
+}
 
 export default async function Page({
   params,
